06711ecb716e8cab13f89090e3726472f7e58ccb
5921001744 <5921001744@SHS-13-221-11.inst.scpsnet.edu>
2019-10-15 09:59:14 -0400

    .

--- src/edu/seminolestate/payments/ManagePayablesApplication.java
+++ src/edu/seminolestate/payments/ManagePayablesApplication.java
@@ -1,5 +1,8 @@
 package edu.seminolestate.payments;
+import edu.seminolestate.bill.*;
+import edu.seminolestate.payable.*;
+import edu.seminolestate.employees.*;
 
 public class ManagePayablesApplication {
-
+	
 }
