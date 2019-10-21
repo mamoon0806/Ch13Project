bb5afbc2bbb7608d4a45b582695c2f4545e2d6fc
5921001744 <5921001744@SHS-13-221-11.inst.scpsnet.edu>
2019-10-21 09:17:58 -0400

    .

--- src/edu/seminolestate/payable/Payable.java
+++
@@ -1,5 +0,0 @@
-package edu.seminolestate.payable;
-
-public interface Payable {
-	public abstract double computeAmountToPay();
-}
--- src/edu/seminolestate/payments/ManagePayablesApplication.java
+++
@@ -1,8 +0,0 @@
-package edu.seminolestate.payments;
-import edu.seminolestate.bill.*;
-import edu.seminolestate.payable.*;
-import edu.seminolestate.employees.*;
-
-public class ManagePayablesApplication {
-	
-}
